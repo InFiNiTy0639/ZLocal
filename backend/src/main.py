64b62e38147d6bec7af9559cd3635f00b4bf5e9b
marq <classicvector63@gmail.com>
2025-06-06 18:26:07 +0530

    add deploy origin

--- backend/src/main.py
+++ backend/src/main.py
@@ -24,7 +24,7 @@ app = FastAPI(title="DeliveryETA", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://zlocal.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
